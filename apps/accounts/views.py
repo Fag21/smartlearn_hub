6cc7368a82784d3685daa89da33607117c529386
Fag21 <filebaralemu@gmail.com>
2025-11-05 12:07:58 -0800

    redirct when the register is seccussful to home page

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -14,13 +14,15 @@ class RegisterView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'accounts/register.html'
-    success_url = reverse_lazy('analytics:dashboard')
+    success_url = reverse_lazy('home')  # Changed from 'analytics:dashboard' to 'home'
     
     def form_valid(self, form):
         response = super().form_valid(form)
         login(self.request, self.object)
         messages.success(self.request, f'Account created for {self.object.username}!')
         return response
+
+# ... rest of the views remain the same
 
 class ProfileView(DetailView):
     model = User
